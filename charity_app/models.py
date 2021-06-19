ea790b93f11a9ae0637674e26dbee60cb52ee784
Wojtek775 <wojciech.kazibet@gmail.com>
2021-06-19 15:39:56 +0200

    add donation models

--- charity_app/models.py
+++ charity_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -20,3 +21,17 @@ class Institution(models.Model):
     description = models.CharField(max_length=64)
     type = models.IntegerField(choices=TYPE)
     categories = models.ManyToManyField('Category')
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField('Category')
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=64)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=128)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
