86f5ec0a92a29109ac6a2140e71858020383ad24
Wojtek775 <wojciech.kazibet@gmail.com>
2021-06-19 17:21:08 +0200

    add count of bags and institution

--- charity_app/views.py
+++ charity_app/views.py
@@ -1,13 +1,20 @@
+from django.db.models import Sum
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
 
+from charity_app.models import Institution, Donation
+
 
 class LandingPage(View):
 
     def get(self, request):
-        return render(request, 'base.html')
+        count_institution = Institution.objects.count()
+        items = Donation.objects.all()
+        count_bags = sum(items.values_list('quantity', flat=True))
+
+        return render(request, 'base.html', {'count': count_institution, 'total': count_bags})
 
 
 class AddDonation(View):
